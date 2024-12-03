dcc5e337f0af98ee4e8d628813ec776bac02426a
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-03 17:48:00 +0530

    Updating the routes and making some new componenets

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout"
 import AuthLogin from "./pages/auth/login"
 import AuthRegister from "./pages/auth/register"
+import AdminLayout from "./components/admin-veiw/layout";
+import AdminDashboard from "./pages/admin-veiw/dashboard";
+import AdminProducts from "./pages/admin-veiw/products";
+import AdminOrders from "./pages/admin-veiw/orders";
+import AdminFeatures from "./pages/admin-veiw/features";
 
 function App() {
   
@@ -12,6 +17,12 @@ function App() {
         <Route path="/auth" element={<AuthLayout/>}>
           <Route path="login" element={<AuthLogin/>}/>
           <Route path="register" element={<AuthRegister/>}/>
+        </Route>
+        <Route path="admin" element={<AdminLayout/>}>
+        <Route path="dashboard" element={<AdminDashboard/>}/>
+        <Route path="products" element={<AdminProducts/>}/>
+        <Route path="orders" element={<AdminOrders/>}/>
+        <Route path="features" element={<AdminFeatures/>}/>
         </Route>
       </Routes>
     </div>
