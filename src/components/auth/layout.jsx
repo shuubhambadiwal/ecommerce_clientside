4a3eaf70c010d98ba0d448f5615566ab3dc8b514
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-01-02 01:52:39 +0530

    CHeck commit

--- src/components/auth/layout.jsx
+++ src/components/auth/layout.jsx
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 
 function AuthLayout() {
     return (
+        // Auth layout
         <div className="flex min-h-screen w-full">
             <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
             <div className="max-w-md space-y-6 text-center text-primary-foreground">
