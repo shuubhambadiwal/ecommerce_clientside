77e74e62b8847a29ddb2e9d15f3840b9169371fe
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-02-01 14:28:39 +0530

    vercal fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,3 +12,4 @@ export default defineConfig({
     },
   },
 })
+
