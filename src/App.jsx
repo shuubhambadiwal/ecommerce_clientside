85e10cb8deb4c3cd98b1311a2ad7e81ecee2d237
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-03-30 14:56:10 +0530

    local code done pushing to cloud

--- src/App.jsx
+++ src/App.jsx
@@ -2,76 +2,95 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
-import AdminLayout from "./components/admin-veiw/layout";
-import AdminDashboard from "./pages/admin-veiw/dashboard";
-import AdminProducts from "./pages/admin-veiw/products";
-import AdminOrders from "./pages/admin-veiw/orders";
-import AdminFeatures from "./pages/admin-veiw/features";
-import ShoppingLayout from "./components/shopping-veiw/layout";
-import ShoppingHome from "./pages/shopping-veiw/home";
-import ShoppingListing from "./pages/shopping-veiw/listing";
-import ShoppingCheckout from "./pages/shopping-veiw/checkout";
-import ShoppingAccount from "./pages/shopping-veiw/account";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
 import UnauthPage from "./pages/unauth-page";
 import { useDispatch, useSelector } from "react-redux";
-import { checkAuth } from "./store/auth-slice";
 import { useEffect } from "react";
-import { Skeleton } from "@/components/ui/skeleton"
-// import NotFound from "./pages/not-found";
+import { checkAuth } from "./store/auth-slice";
+import { Skeleton } from "@/components/ui/skeleton";
+import PaypalReturnPage from "./pages/shopping-view/paypal-return";
+import PaymentSuccessPage from "./pages/shopping-view/payment-success";
+import SearchProducts from "./pages/shopping-view/search";
 
 function App() {
-  // const user = null;
-  const{user, isAuthenticated, isLoading} = useSelector(state => state.auth);
-
+  const { user, isAuthenticated, isLoading } = useSelector(
+    (state) => state.auth
+  );
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(checkAuth());
   }, [dispatch]);
 
-  if(isLoading) {
-    return <Skeleton className="w-[600px] bg-black h-[600px] "/>;
-  }
+  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
 
-  // console.log(isLoading, user);
+  console.log(isLoading, user);
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      {/* <h1>Header component</h1> */}
       <Routes>
-        {/* Routing for the Login and Register÷ */}
         <Route
-          path="/auth" 
+          path="/"
+          element={
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}
+            ></CheckAuth>
+          }
+        />
+        <Route
+          path="/auth"
           element={
             <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <AuthLayout />
             </CheckAuth>
-          }>
+          }
+        >
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        {/* Routing for the Admin Layout */}
-        <Route path="admin" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <AdminLayout />
-          </CheckAuth>
-          }>
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
-        {/* Routing for the Shopping Layout   */}
-        <Route path="/shop" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-             <ShoppingLayout />
-          </CheckAuth>
-          }>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
+          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
-        <Route path="/unauth-page" element={<UnauthPage/>} />
+        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { Toaster } from "@/components/ui/toaster"
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
-      <Toaster/>
+      <Toaster />
     </Provider>
   </BrowserRouter>
 );
