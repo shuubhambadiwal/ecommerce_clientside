bcc3a811daa391ea379e045a85038557dc91f44d
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-03-30 17:01:19 +0530

    update the api paths

--- src/components/admin-view/image-upload.jsx
+++ src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5001/api/admin/products/upload-image",
+      "http://ecomser.vercel.app/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/register",
+      "http://ecomser.vercel.app/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/login",
+      "http://ecomser.vercel.app/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "http://localhost:5001/api/auth/logout",
+      "http://ecomser.vercel.app/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "http://localhost:5001/api/auth/check-auth",
+      "http://ecomser.vercel.app/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
